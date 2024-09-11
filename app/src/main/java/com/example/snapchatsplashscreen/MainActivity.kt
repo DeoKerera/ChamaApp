4ad0204bda3f2afc1a7fc59d30f092d930e57ffa
Kerera Deo <kerera233@gmail.com>
2024-09-11 18:27:51 +0300

    Second commit with login

--- app/src/main/java/com/example/snapchatsplashscreen/MainActivity.kt
+++ app/src/main/java/com/example/snapchatsplashscreen/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.snapchatsplashscreen
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,10 +14,16 @@ class MainActivity : AppCompatActivity() {
         Thread.sleep(300)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        var btn1=findViewById<Button>(R.id.btn1)
+        btn1.setOnClickListener {
+            val intent1=Intent(this,LoginPage::class.java)
+            startActivity(intent1)
         }
-    }
-}
+        var btn2=findViewById<Button>(R.id.btn2)
+        btn2.setOnClickListener {
+            val intent2=Intent(this,RegisterPage::class.java)
+            startActivity(intent2)
+        }
+
+        }
+        }
